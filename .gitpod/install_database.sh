1db2b51412c6647b7966ce241093f368df954bb4
Jason Evans <evans022@gmail.com>
2022-11-02 22:54:19 -0500

    Created ddev and gitpod configuration files.

---
+++ .gitpod/install_database.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -eu
+
+url=$(gp url | awk -F"//" {'print $2'}) && url+="/" &&
+url="https://8080-"$url"/"
+
+cd ${GITPOD_REPO_ROOT}
+
+if [ "${INSTALL_MAGENTO}" = "YES" ]; then
+  ddev magento setup:install --db-name='db' --db-user='db' --db-password='db' --base-url=$url --backend-frontname='admin' --admin-user=$MAGENTO_ADMIN_USERNAME --admin-password=$MAGENTO_ADMIN_PASSWORD --admin-email=$GITPOD_GIT_USER_EMAIL --admin-firstname='Admin' --admin-lastname='User' --use-rewrites='1' --use-secure='1' --base-url-secure=$url --use-secure-admin='1' --language='en_US' --db-host='127.0.0.1' --cleanup-database --timezone='America/Chicago' --currency='USD' --session-save='redis';
+fi
+
+if [ "${INSTALL_SAMPLE_DATA}" = "YES" ]; then
+  ddev magento sampledata:deploy;
+fi
+
+if [ "${INSTALL_MAGENTO}" = "NO" ] && [ -f ".gitpod/magento-db.sql.zip" ]; then
+  ddev import-db --src=.gitpod/magento-db.sql.zip
+else
+  echo "No database was imported. No .gitpod/magento-db.sql.zip was provided and magedbm2 was not configured."
+fi
+if [ -f ".gitpod/files.tgz" ]; then
+  ddev import-files --src=.gitpod/files.tgz
+else
+  echo "No files.tgz was provided in .gitpod"
+fi
+
+ddev magento setup:upgrade
+ddev magento config:set web/cookie/cookie_path "/"
+ddev magento config:set web/cookie/cookie_domain ".gitpod.io"
+ddev magento setup:store-config:set --base-url="${url}"
+ddev magento setup:store-config:set --base-url-secure="${url}"
+ddev magento setup:config:set --session-save=redis --session-save-redis-host=redis --session-save-redis-log-level=3 --session-save-redis-db=0 --session-save-redis-port=6379;
+ddev magento setup:config:set --cache-backend=redis --cache-backend-redis-server=redis --cache-backend-redis-db=1;
+ddev magento setup:config:set --page-cache=redis --page-cache-redis-server=redis --page-cache-redis-db=2;
+ddev magento module:disable Magento_Csp Magento_TwoFactorAuth
+ddev magento cache:flush
+ddev redis-cli flushall
+
+touch ${GITPOD_REPO_ROOT}/.gitpod/db-installed.flag
+gp ports await 8080 && sleep 1 && gp preview $(gp url 8080)
---
+++ .gitpod/start_repo.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -eu
+
+url=$(gp url | awk -F"//" {'print $2'}) && url+="/" &&
+url="https://8080-"$url"/"
+
+cd ${GITPOD_REPO_ROOT}
+# Temporarily use an empty config.yaml to get ddev to use defaults
+# so we can do composer install. If there's already one there,
+# this does no harm.
+mkdir -p .ddev && touch .ddev/config.yaml
+
+# If there's a composer.json, do `ddev composer install` (which auto-starts projct)
+if [ -f composer.json ]; then
+  ddev start
+  ddev composer install
+else
+  composer config -g -a http-basic.repo.magento.com ${MAGENTO_COMPOSER_AUTH_USER} ${MAGENTO_COMPOSER_AUTH_PASS}
+  composer create-project --no-interaction --no-progress --repository-url=https://repo.magento.com/ magento/project-${MAGENTO_EDITION}-edition=${MAGENTO_VERSION} magento2
+  cd magento2 && cp -avr .* ${GITPOD_REPO_ROOT};
+  cd ${GITPOD_REPO_ROOT} && rm -r -f magento2 && git checkout -- .gitignore;
+fi
+
+if [ ! -f bin/magerun2 ]; then
+  ddev exec curl -L https://files.magerun.net/n98-magerun2.phar --output bin/magerun2
+  ddev exec chmod +x bin/magerun2
+fi
+
+ddev get drud/ddev-redis
+ddev get drud/ddev-elasticsearch
+ddev get drud/ddev-cron
+
+# This won't be required in ddev v1.18.2+
+printf "host_webserver_port: 8080\nhost_https_port: 2222\nhost_db_port: 3306\nhost_mailhog_port: 8025\nhost_phpmyadmin_port: 8036\nbind_all_interfaces: true\n" >.ddev/config.gitpod.yaml
+ddev stop -a
+ddev start -y
